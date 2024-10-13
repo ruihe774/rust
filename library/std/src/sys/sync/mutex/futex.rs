7cd5e6a97af2522d3d21676b9639fe8ada62b950
Misaki Kasumi <misakikasumi@outlook.com>
2024-10-13 19:08:07 +0800

    Add futex requeue for OpenBSD

--- library/std/src/sys/sync/mutex/futex.rs
+++ library/std/src/sys/sync/mutex/futex.rs
@@ -96,6 +96,7 @@ impl Mutex {
     }
 }
 
+#[cfg(not(target_os = "openbsd"))]
 mod fallback_requeuer {
     use crate::sync::atomic::AtomicU32;
     use crate::sys::futex::{futex_wait, futex_wake, futex_wake_all};
@@ -136,4 +137,53 @@ mod fallback_requeuer {
     }
 }
 
+#[cfg(not(target_os = "openbsd"))]
 pub use fallback_requeuer::Requeuer;
+
+#[cfg(target_os = "openbsd")]
+mod futex_requeuer {
+    use super::Mutex;
+    use crate::sync::atomic::AtomicU32;
+    use crate::sys::futex::{futex_requeue, futex_wait, futex_wake};
+    use crate::time::Duration;
+
+    pub struct Requeuer(Mutex);
+
+    impl Requeuer {
+        #[inline]
+        pub const fn new() -> Requeuer {
+            Requeuer(Mutex::new())
+        }
+
+        #[inline]
+        pub fn wake_one(&self, expected: u32, from: &AtomicU32) {
+            let _ = expected;
+            futex_wake(from);
+        }
+
+        #[inline]
+        pub fn wake_one_and_requeue_other(&self, expected: u32, from: &AtomicU32) {
+            let _ = expected;
+            futex_requeue(from, &self.0.futex);
+            self.0.lock();
+        }
+
+        #[inline]
+        pub fn wait_requeuable(
+            &self,
+            expected: u32,
+            from: &AtomicU32,
+            timeout: Option<Duration>,
+        ) -> bool {
+            futex_wait(from, expected, timeout)
+        }
+
+        #[inline]
+        pub unsafe fn wake_another(&self) {
+            self.0.wake();
+        }
+    }
+}
+
+#[cfg(target_os = "openbsd")]
+pub use futex_requeuer::Requeuer;
